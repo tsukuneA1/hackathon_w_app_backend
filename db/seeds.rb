ea84832489e3d2238d0499fcb8f28b535ca3e9af
SaeYoshizaki <yoshizakisae@gmail.com>
2025-08-14 17:33:25 +0900

    ダミーのseedデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,29 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.create!(
+  provider: "github",
+  uid: "444444",
+  username: "Dan",
+  email: "dan.reynolds@example.com",
+  name: "Daniel Coulter Reynolds",
+  avatar_url: "https://avatars.githubusercontent.com/u/123456789?v=4",
+  last_login_at: Time.current
+)
+
+User.create!(
+  provider: "github",
+  uid: "555555",
+  username: "Wayne",
+  email: nil,
+  name: "Wayne Sermon",
+  avatar_url: "https://avatars.githubusercontent.com/u/987654321?v=4",
+  last_login_at: nil
+)
+
+User.create!(
+  provider: "github",
+  uid: "666666",
+  username: "Ben",
+  email: "ben.mckee@example.com",
+  name: "Benjamin Arthur McKee",
+  avatar_url: nil,
+  last_login_at: Time.current
+)
