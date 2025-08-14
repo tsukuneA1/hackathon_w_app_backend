2fee5ee27b4831565ac7657d60d72adcddc8e653
SaeYoshizaki <yoshizakisae@gmail.com>
2025-08-14 21:50:24 +0900

    github copilot reviewで指摘された部分を修正

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_14_020000) do
     t.string "email"
     t.string "name"
     t.string "avatar_url"
-    t.datetime "last_login_at", default: -> { 'CURRENT_TIMESTAMP' }
+    t.datetime "last_login_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true, where: "(email IS NOT NULL)"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,30 @@
-User.create!(
+User.find_or_create_by!(
   provider: "github",
-  uid: "444444",
-  username: "Dan",
-  email: "dan.reynolds@example.com",
-  name: "Daniel Coulter Reynolds",
-  avatar_url: "https://avatars.githubusercontent.com/u/123456789?v=4",
-  last_login_at: Time.current
-)
-
-User.create!(
+  uid: "444444"
+) do |user|
+  user.username = "Dan"
+  user.email = "dan.reynolds@example.com"
+  user.name = "Daniel Coulter Reynolds"
+  user.avatar_url = "https://avatars.githubusercontent.com/u/123456789?v=4"
+  user.last_login_at = Time.current
+end
+User.find_or_create_by!(
   provider: "github",
-  uid: "555555",
-  username: "Wayne",
-  email: nil,
-  name: "Wayne Sermon",
-  avatar_url: "https://avatars.githubusercontent.com/u/987654321?v=4",
-  last_login_at: nil
-)
-
-User.create!(
+  uid: "555555"
+) do |user|
+  user.username = "Wayne"
+  user.email = nil
+  user.name = "Wayne Sermon"
+  user.avatar_url = "https://avatars.githubusercontent.com/u/987654321?v=4"
+  user.last_login_at = nil
+end
+User.find_or_create_by!(
   provider: "github",
-  uid: "666666",
-  username: "Ben",
-  email: "ben.mckee@example.com",
-  name: "Benjamin Arthur McKee",
-  avatar_url: nil,
-  last_login_at: Time.current
-)
+  uid: "666666"
+) do |user|
+  user.username = "Ben"
+  user.email = "ben.mckee@example.com"
+  user.name = "Benjamin Arthur McKee"
+  user.avatar_url = nil
+  user.last_login_at = Time.current
+end
