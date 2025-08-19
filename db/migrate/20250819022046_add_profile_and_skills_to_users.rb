13c06ec3b5aab66632fff15f6a74dd4d8c4840b3
SaeYoshizaki <yoshizakisae@gmail.com>
2025-08-19 14:53:29 +0900

    エラー部分＋フォーマットエラー修正

--- db/migrate/20250819022046_add_profile_and_skills_to_users.rb
+++ db/migrate/20250819022046_add_profile_and_skills_to_users.rb
@@ -4,7 +4,7 @@ class AddProfileAndSkillsToUsers < ActiveRecord::Migration[8.0]
     add_column :users, :avatar_url, :string unless column_exists?(:users, :avatar_url)
 
     add_column :users, :bio, :text unless column_exists?(:users, :bio)
-    add_column :users, :location, :string column_exists?(:users, :location)
+    add_column :users, :location, :string  unless column_exists?(:users, :location)
 
     add_column :users, :skills, :jsonb, null: false, default: {} unless column_exists?(:users, :skills)
     add_column :users, :role, :integer, null: false, default: 0 unless column_exists?(:users, :role)
@@ -15,8 +15,8 @@ class AddProfileAndSkillsToUsers < ActiveRecord::Migration[8.0]
     add_column :users, :last_login_at, :string unless column_exists?(:users, :last_login_at)
 
     add_index :users, :skills, using: :gin unless index_exists?(:users, :skills, using: :gin)
-    add_index :users, [:oauth_provider, :oauth_uid], unique:true,
-              name: "index_users_on_oauth_provider_and_uid" unless index_exists?(users, [:aouth_provider, :oauth_uid], name: "index_users_on_oauth_provider_and_uid")
-    add_index :users, :username, unique:true unless index_exists?(:users, :username)
+    add_index :users, [ :oauth_provider, :oauth_uid ], unique: true,
+              name: "index_users_on_oauth_provider_and_uid" unless index_exists?(users, [ :aouth_provider, :oauth_uid ], name: "index_users_on_oauth_provider_and_uid")
+    add_index :users, :username, unique: true unless index_exists?(:users, :username)
   end
 end
