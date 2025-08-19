85f0b2db271ff69e1917244b5145c4a4c3e15539
SaeYoshizaki <yoshizakisae@gmail.com>
2025-08-19 11:48:03 +0900

    コントローラーの実装

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,26 @@
-class Api::V1::UsersController < ApplicationController
+module Api
+    module V1
+        class Api::V1::UsersController < ApplicationController
+            def index
+                users = User.order(created_at: :desc)
+                render json: users.as_json(only:visible_fields)
+            end
+        
+        def show
+            user = User.find(params[:id])
+            render json: user.as_json(only: visible_fields)
+        rescue ActiveRecord::RecordNotFound
+            render json: {error: "ユーザーが見つかりません"}, status: :not_found
+        end
+
+        private
+
+        def visible_fields
+            %i[
+                id provider uid username name avatar_url bio location skills last_login_at role created_at updated_at
+            ]
+        end
+    end
+end
+
 end
