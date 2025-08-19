0bce6ceb9c5c8ad8a636637906c3426ce14d81b1
SaeYoshizaki <yoshizakisae@gmail.com>
2025-08-19 11:49:18 +0900

    タイプミスや文法の誤りを修正

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,14 +3,14 @@ module Api
         class Api::V1::UsersController < ApplicationController
             def index
                 users = User.order(created_at: :desc)
-                render json: users.as_json(only:visible_fields)
+                render json: users.as_json(only: visible_fields)
             end
-        
+
         def show
             user = User.find(params[:id])
             render json: user.as_json(only: visible_fields)
         rescue ActiveRecord::RecordNotFound
-            render json: {error: "ユーザーが見つかりません"}, status: :not_found
+            render json: { error: "ユーザーが見つかりません" }, status: :not_found
         end
 
         private
@@ -20,7 +20,6 @@ module Api
                 id provider uid username name avatar_url bio location skills last_login_at role created_at updated_at
             ]
         end
+        end
     end
-end
-
 end
