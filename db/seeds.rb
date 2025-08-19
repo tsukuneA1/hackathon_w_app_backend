c82171d7ac4c2f37b6ae8f830409c9783ff22513
SaeYoshizaki <yoshizakisae@gmail.com>
2025-08-19 15:00:49 +0900

    一旦push

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+users = [
+    {
+        provider: "github",
+        uid: "10001",
+        username: "Sum"
+        avatar_url: "https://avatars.githubusercontent.com/u/583231",
+        bio: ""
+    }
+]
