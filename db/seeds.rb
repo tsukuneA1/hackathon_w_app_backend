2db4da520021c507a47785a2a3366235aa5d2c4c
SaeYoshizaki <yoshizakisae@gmail.com>
2025-08-19 18:23:34 +0900

    seedを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-users = [
-    {
-        provider: "github",
-        uid: "10001",
-        username: "Sum"
-        avatar_url: "https://avatars.githubusercontent.com/u/583231",
-        bio: ""
-    }
-]
+User.create!([
+  {
+    provider: "github",
+    uid: "10001",
+    username: "Sum",
+    avatar_url: "https://avatars.githubusercontent.com/u/583231",
+    bio: ""
+  },
+  {
+    provider: "github",
+    uid: "10002",
+    username: "Tom",
+    avatar_url: "https://avatars.githubusercontent.com/u/10002",
+    bio: "Pythonが得意です！"
+  }
+])
